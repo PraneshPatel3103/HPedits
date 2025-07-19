7d6f82dd3b37c2994898a925ef799518211b31f1
PraneshPatel3103 <patelpranesh95@gmail.com>
2025-07-20 02:22:56 +0530

    Use dj-database-url for Railway DATABASE_URL

--- photo_gallery/settings.py
+++ photo_gallery/settings.py
@@ -47,12 +47,12 @@ TEMPLATES = [{
 
 WSGI_APPLICATION = 'photo_gallery.wsgi.application'
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
 }
+
 
 AUTH_PASSWORD_VALIDATORS = []
 
